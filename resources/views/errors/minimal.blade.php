2a9a5c82fbfde87dae39b541db1138f313fa646f
Emerson Cedeño <ecedenyo@gmail.com>
2021-08-29 00:49:59 -0500

    🚸 Adding nav to Home on HTTP Error pages

--- resources/views/errors/minimal.blade.php
+++ resources/views/errors/minimal.blade.php
@@ -24,12 +24,13 @@
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
             <div class="max-w-xl mx-auto sm:px-6 lg:px-8">
                 <div class="flex items-center pt-8 sm:justify-start sm:pt-0">
-                    <div class="px-4 text-lg text-gray-500 border-r border-gray-400 tracking-wider">
+                    <div class="px-4 py-4 text-lg text-gray-500 border-r border-gray-400 tracking-wider">
                         @yield('code')
                     </div>
 
-                    <div class="ml-4 text-lg text-gray-500 uppercase tracking-wider">
-                        @yield('message')
+                    <div class="ml-4 text-gray-500">
+                        <span class="text-lg uppercase tracking-wider">@yield('message')</span><br>
+                        <a href="/" class="text-sm" title="Go Home">« Home</a>
                     </div>
                 </div>
             </div>
